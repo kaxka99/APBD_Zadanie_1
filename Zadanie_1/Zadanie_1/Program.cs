3f73bf95e9a0ee480738491215cb357489395b5d
s21383 <s21383@pjwstk.edu.pl>
2024-03-11 10:55:49 +0100

    Dodana metoda average

--- Zadanie_1/Zadanie_1/Program.cs
+++ Zadanie_1/Zadanie_1/Program.cs
@@ -7,5 +7,18 @@ public class Program
         Console.WriteLine("Hello World");
 
         Console.WriteLine("Goodbye World!");
+
+    }
+
+    public static float average(int[] intArray)
+    {
+        float sum = 0.0f, average = 0.0f;
+        foreach(int i in intArray)
+        {
+            sum += i;
+        }
+        average = sum / intArray.Length;
+
+        return average;
     }
 }
