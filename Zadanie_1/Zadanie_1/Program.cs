02812d4bc6e33f89b6323a93f52cfc383d96d0b8
s21383 <s21383@pjwstk.edu.pl>
2024-03-11 10:58:50 +0100

    Modyfikacja 4

--- Zadanie_1/Zadanie_1/Program.cs
+++ Zadanie_1/Zadanie_1/Program.cs
@@ -7,5 +7,20 @@ public class Program
         Console.WriteLine("Hello World");
 
         Console.WriteLine("Goodbye World!");
+
+        int[] inputArray = { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine(average(inputArray));
+    }
+
+    public static float average(int[] intArray)
+    {
+        float sum = 0.0f, average = 0.0f;
+        foreach(int i in intArray)
+        {
+            sum += i;
+        }
+        average = sum / intArray.Length;
+
+        return average;
     }
 }
